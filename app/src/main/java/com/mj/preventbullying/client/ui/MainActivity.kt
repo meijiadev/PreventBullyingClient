b64eebfd24405408c681daf53dc384a1084a6696
meijiadev <1050013833@qq.com>
2023-12-09 17:27:29 +0800

    新增主页UI

--- app/src/main/java/com/mj/preventbullying/client/ui/MainActivity.kt
+++ app/src/main/java/com/mj/preventbullying/client/ui/MainActivity.kt
@@ -10,10 +10,11 @@ import com.sjb.base.base.BaseMvActivity
 import java.util.UUID
 
 /**
- * 主页
+ * Create by MJ on 2023/12/9.
+ * Describe : 主页
  */
 class MainActivity : BaseMvActivity<ActivityMainBinding, LoginViewModel>() {
-    private var randomStr: String? = null
+
     override fun getViewBinding(): ActivityMainBinding {
        return ActivityMainBinding.inflate(layoutInflater)
     }
@@ -31,19 +32,6 @@ class MainActivity : BaseMvActivity<ActivityMainBinding, LoginViewModel>() {
     }
 
     override fun initView() {
-        postDelayed({
-            Logger.i("网络是否可以：${NetworkUtil.isAvailable()},IP:${NetworkUtil.getIPAddress(true)}")
-            randomStr = UUID.randomUUID().toString()
-            val url = "http://192.168.1.6:9999/code?randomStr=123456"
-            //val url="https://login.sina.com.cn/cgi/pin.php?r=9967937&s=0&p=gz-d0dc363f6a4523cbd602a5a10f00c59b"
-            binding.codeImage.load(url){
-                error(R.drawable.ic_launcher_background)
-            }
-//            Glide.with(this)
-//                .load(url)
-//                .into(binding.codeImage!!)
-            Logger.i("加载验证码：$url")
-        }, 1500)
     }
 
     override fun initListener() {
---
+++ app/src/main/java/com/mj/preventbullying/client/ui/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.mj.preventbullying.client.ui
+
+import com.sjb.base.base.BaseViewModel
+
+/**
+ * Create by MJ on 2023/12/9.
+ * Describe : 主页的viewModel
+ */
+class MainViewModel :BaseViewModel() {
+
+}
